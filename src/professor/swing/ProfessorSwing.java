9a065a9a089502c45238be43c8da96b4f33375d5
babiprocs <gabrielihendges@gmail.com>
2016-11-24 11:57:54 -0200

    Alterações :sunny: :cat:

--- src/professor/swing/ProfessorSwing.java
+++ src/professor/swing/ProfessorSwing.java
@@ -115,12 +115,13 @@ public class ProfessorSwing extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTRanking = new javax.swing.JTable();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem4 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem5 = new javax.swing.JMenuItem();
         jMenu3 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setSize(new java.awt.Dimension(934, 565));
@@ -609,26 +610,13 @@ public class ProfessorSwing extends javax.swing.JFrame {
         );
         jPRankingLayout.setVerticalGroup(
             jPRankingLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 499, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 501, Short.MAX_VALUE)
         );
 
         jTFundo.addTab("Ranking", new javax.swing.ImageIcon(getClass().getResource("/imagens/vintequatro/trophy-variant.png")), jPRanking); // NOI18N
 
         jMenuBar1.setBackground(new java.awt.Color(1, 1, 1));
         jMenuBar1.setForeground(new java.awt.Color(254, 254, 254));
-
-        jMenu1.setText("Opções");
-
-        jMenuItem4.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_I, java.awt.event.InputEvent.CTRL_MASK));
-        jMenuItem4.setText("Informações GamesApp");
-        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem4ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem4);
-
-        jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Atividades");
 
@@ -656,6 +644,22 @@ public class ProfessorSwing extends javax.swing.JFrame {
 
         jMenuBar1.add(jMenu3);
 
+        jMenu1.setText("Opções");
+
+        jMenuItem4.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_I, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem4.setText("Informações GamesApp");
+        jMenuItem4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem4ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem4);
+
+        jMenuItem2.setText("Alterar senha");
+        jMenu1.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu1);
+
         setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -667,7 +671,7 @@ public class ProfessorSwing extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jTFundo, javax.swing.GroupLayout.PREFERRED_SIZE, 543, Short.MAX_VALUE)
+                .addComponent(jTFundo, javax.swing.GroupLayout.PREFERRED_SIZE, 545, Short.MAX_VALUE)
                 .addGap(1, 1, 1))
         );
 
@@ -869,6 +873,7 @@ public class ProfessorSwing extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JPanel jPAtividades;
